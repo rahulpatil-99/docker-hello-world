05cd3b0e2141d8cb09477a2c922f1c484a42540c
Rahul Patil <rp7187655@gmail.com>
2019-02-07 09:27:12 +0530

    add todos endpoint to consume other endpoint

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const http = require('http');
+const request = require('request');
 const app = express();
 
+let todoPath = process.env.url;
+
 app.get("/", (req,res)=> res.send("Hello"));
+
+app.get("/todos", (req,res)=> {
+    request.get(`${todoPath}/todos`, (error, response, body)=>{
+        res.send(body);
+    });
+});
 
 http.createServer(app).listen(3000);
